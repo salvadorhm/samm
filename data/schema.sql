f972f1cb34f9ff6fca13e337efad575d1db664b3
Salvador Hernandez Mendoza <salvadorhm@gmail.com>
2019-06-21 15:30:12 -0500

    base modificada

--- data/schema.sql
+++ data/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE telegram_users(
 
 CREATE TABLE productos(
   id_producto int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  sku varchar(10) INDEX NOT NULL,
+  sku varchar(10) NOT NULL,
   producto varchar(100) NOT NULL,
   precio float NOT NULL,
   existencias int(11) NOT NULL,
@@ -29,11 +29,11 @@ CREATE TABLE answers_logs(
     time_update timestamp
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-INSERT INTO answers(answer_key,answer_value,answer_type) 
-VALUES 
-('001','Este es un texto',1),
-('002','imagen1.jpg',2),
-('003','archivo1.pdf',3);
+INSERT INTO productos(sku, producto, precio, existencias)
+VALUES
+('mar001','Martillo Rojo',100.00, 12),
+('mar002','Martillo Amarillo',110.00, 13),
+('tal001','Taladro',1000.00, 5);
 
 CREATE USER 'samm'@'localhost' IDENTIFIED BY 'samm.2019';
 GRANT ALL PRIVILEGES ON samm_db.* TO 'samm'@'localhost';
